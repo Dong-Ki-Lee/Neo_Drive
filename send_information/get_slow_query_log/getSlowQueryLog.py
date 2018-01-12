3405c4cde889a9b640691b5b3663244b0490bbbe
Dong-Ki-Lee <dlehdrl9638@gmail.com>
2018-01-12 14:10:36 +0900

    add get slow query log beta

---
+++ send_information/get_slow_query_log/getSlowQueryLog.py
@@ -0,0 +1,31 @@
+import pymysql
+import logstash
+import logging
+import time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+# if use intranet only, use host name monitoring server
+ip = s.getsockname()[0]
+ip_tuple = (('CLIENT_IP', ip),)
+print(ip)
+s.close()
+
+conn = pymysql.connect(host='localhost', user='monitoring', password='monitoringtest', charset='utf8')
+
+test_logger = logging.getLogger('python-logstash-logger')
+test_logger.setLevel(logging.INFO)
+test_logger.addHandler(logstash.LogstashHandler("164.125.14.150", 5958, version=1))
+
+curs = conn.cursor()
+while True:
+    sql = 'select * from mysql.slow_log'
+    curs.execute(sql)
+    rows = curs.fetchall()
+#    test_logger.info(rows+ip_tuple)
+    print(rows)
+
+    time.sleep(5)
+
+conn.close()
