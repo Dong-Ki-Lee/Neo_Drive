13838176e2695b6db9bfe98e7d6ca9f8db1b7ff7
Dong-Ki-Lee <dlehdrl9638@gmail.com>
2018-02-05 18:26:58 +0900

    patch system information sending module

--- send_information/get_system_info/system_information_sending_module.py
+++ send_information/get_system_info/system_information_sending_module.py
@@ -5,42 +5,65 @@ import psutil
 import socket
 from pymongo import MongoClient
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-# if use intranet only, use host name monitoring server
-ip = s.getsockname()[0]
-print(ip)
-s.close()
 
-server_ip = "164.125.14.150"
+MONITORING_SERVER_IP = '164.125.14.150'
+MONITORING_SERVER_MONGODB_PORT = 26543
+MONITORING_SERVER_SYSTEM_INFO_LOGSTASH_PORT = 5959
+MONITORING_SERVER_ERROR_INFO_LOGSTASH_PORT = 5957
 
-client = MongoClient(server_ip, 26543)
-db = client.client_system_limit_error
-db_information = client.client_access_log
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    # if use intranet only, use host name monitoring server
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+def insert_data_in_mongodb(coll_name, json_data):
+    is_success = 1
+    try:
+        client = MongoClient(MONITORING_SERVER_IP, MONITORING_SERVER_MONGODB_PORT)
+        db = client.server_data
+        collection = db[coll_name]
+        collection.insert_one(json_data)
+        client.close()
+    except:
+        is_success = 0
+    finally:
+        return is_success
+
 
 system_information_logger = logging.getLogger('system_information_logger')
 system_information_logger.setLevel(logging.INFO)
-system_information_logger.addHandler(logstash.LogstashHandler(server_ip, 5959, version=1))
+system_information_logger.addHandler(logstash.LogstashHandler(
+    MONITORING_SERVER_IP,
+    MONITORING_SERVER_SYSTEM_INFO_LOGSTASH_PORT,
+    version=1)
+)
 
 system_error_logger = logging.getLogger('error_logger')
 system_error_logger.setLevel(logging.INFO)
-system_error_logger.addHandler(logstash.LogstashHandler(server_ip, 5957, version=1))
-
-print(psutil.cpu_percent())
+system_error_logger.addHandler(logstash.LogstashHandler(
+    MONITORING_SERVER_IP,
+    MONITORING_SERVER_ERROR_INFO_LOGSTASH_PORT,
+    version=1)
+)
 
 network = psutil.net_io_counters().packets_recv
 cpu_usage_limit = input("input cpu usage limit this system : ")
 cpu_time_limit = input("input cpu time limit this system : ")
-#network_limit = input("input network limit this system : ")
 
 cpu_over_limit_time = 0
-
-coll_information = db_information["access_log"]
+ip = get_ip_address()
 client_information = {
     'IP': ip,
+    'type': 'connect',
     'access_time': time.asctime(time.localtime())
 }
-coll_information.insert_one(client_information)
+
+insert_data_in_mongodb("access_log", client_information)
 
 while(True):
     cpu = psutil.cpu_percent()
@@ -50,10 +73,10 @@ while(True):
     network = psutil.net_io_counters().packets_recv
     network_usage = int((network - network_usage) / 5 / 10240)
 
-    obj = str(cpu) + ' ' + str(mem) + ' ' + str(disk) + ' ' + str(network_usage) + ' ' + cpu_usage_limit + ' ' + ip
-    print(obj)
+    system_information = str(cpu) + ' ' + str(mem) + ' ' + str(disk) + ' ' + str(network_usage) + ' ' + cpu_usage_limit + ' ' + ip
+    print(system_information)
 #    psutil.test()
-    system_information_logger.info(obj)
+    system_information_logger.info(system_information)
 
     print("insert one data")
 
@@ -61,14 +84,15 @@ while(True):
         if int(cpu_over_limit_time) < int(cpu_time_limit):
             cpu_over_limit_time += 1
         else:
-            coll = db["cpu_over_limit"]
             t = time.localtime()
-            obj = {
-                'IP': ip,
-                'occur_time': time.asctime(t),
-                'error_name': 'CPU_Usage_over_limit'
+            error_information = {
+                'error_name': 'CPU_Usage_over_limit',
+                'error_time': time.asctime(t),
+                'error_ip': ip
             }
-            coll.insert_one(obj)
+
+            insert_data_in_mongodb("error_log", error_information)
+
             elastic_obj = ip + ' ' + 'CPU_Usage_over_limit'
             system_error_logger.info(elastic_obj)
             print("send cpu limit error")
